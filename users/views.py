9a2f0ef01205f1dbc2adb6b6f0bc0c0c541262cf
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-27 11:25:32 -0300

    feat: user views done

--- users/views.py
+++ users/views.py
@@ -1,4 +1,42 @@
 from rest_framework import generics
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView, Response, status
+
+from .serializers import LoginSerializer, UserSerializer
+
+from .models import User
 
 
-class 
+class ListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ListUsersDateJoinedView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+      num_users = self.kwargs["num"]
+      return self.queryset.order_by("-date_joined")[0:num_users]
+
+class LoginView(APIView):
+    def post(self, request): 
+        serializer = LoginSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        email = serializer.validated_data["email"]
+        password = serializer.validated_data["password"]
+
+        user = authenticate(username=email, password=password)
+
+        if user: 
+            token, _ = Token.objects.get_or_create(user=user)
+
+            return Response({"token": token.key}, status.HTTP_200_OK)
+
+        return Response(
+            {"detail": "invalid email or password"},
+            status.HTTP_401_UNAUTHORIZED
+        )
