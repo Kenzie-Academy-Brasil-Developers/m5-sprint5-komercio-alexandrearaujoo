651c3d70dec6a5946b0b37c579f470ee4ce8b066
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-28 11:43:16 -0300

    feat: product views done

--- products/views.py
+++ products/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+from .models import Product
+from .serializers import PostProductSerializer, GetProductsSerializer
+from .mixins import SerializerByMethodMixin
+from .permissions import ProductPermissionsCustom
+
+class ListCreateView(SerializerByMethodMixin,generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_map = {
+        'GET': GetProductsSerializer,
+        'POST': PostProductSerializer
+    }
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [ProductPermissionsCustom]
+
+    def perform_create(self, serializer):
+        serializer.save(seller=self.request.user)
+
+class RetriveUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_map = {
+        'GET': GetProductsSerializer,
+        'PATCH': PostProductSerializer
+    }
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [ProductPermissionsCustom]
