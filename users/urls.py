d40fe92cbb6b28435937d25b5eda98a17a134a6b
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-29 11:09:06 -0300

    feat: add update url

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from users import views
 urlpatterns = [
     path('accounts/', views.ListCreateView.as_view()),
     path('login/', views.LoginView.as_view()),
-    path('accounts/newest/<int:num>/', views.ListUsersDateJoinedView.as_view())
+    path('accounts/newest/<int:num>/', views.ListUsersDateJoinedView.as_view()),
+    path('accounts/<pk>', views.UpdateUserView.as_view())
 ]
