bc13c980f9ff059c4c41d9dd09a0a1702ab988d3
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-30 16:16:53 -0300

    fix: change quantity field type

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10,decimal_places=2) 
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField()
     is_active = models.BooleanField(default=True)
 
     seller = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='products')
